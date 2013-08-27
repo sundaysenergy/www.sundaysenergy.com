2c8c48143d1eac028de7cc78335891d20c43bc88
Tim Lupfer <tim@sundaysenergy.com>
2013-08-27 15:16:37 -0500

    Begin work on new pricing page

---
+++ theme/javascripts/pricing.js
@@ -0,0 +1,13 @@
+$('document').ready(function() {
+  $('#get_drupal_hosting').on('click', function(e) {
+    e.preventDefault();
+    $('#drupal_hosting').modal();
+  });
+  $('.icon-github').on('click', function(e) {
+    e.preventDefault();
+    OAuth.popup('github', function(err, result) {
+      $('#drupal_hosting .modal-body').css('font-size', '1.3em');
+      $('#drupal_hosting .modal-body').html(result.access_token);
+    });
+  });
+});
