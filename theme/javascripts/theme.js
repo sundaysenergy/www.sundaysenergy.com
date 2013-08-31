ff267a7f3be93dec58171c8821b5005491da9ca4
Kai Curry <kai@sundaysenergy.com>
2013-08-30 21:48:30 -0500

    hide if have uid over 0

--- theme/javascripts/theme.js
+++ theme/javascripts/theme.js
@@ -44,3 +44,30 @@ $(function () {
     }
     OAuth.initialize('aSOjwnUIZ2k-r1gYcA1lix_Fc9s');
 });
+
+function getCookie(name) {
+  var parts = document.cookie.split(name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+$('document').ready(function() {
+  if (0 != parseInt(getCookie('uid'))) {
+    $('#login').hide();
+  }
+  $('#login').on('click', function(e) {
+    console.log("got here!");
+    e.preventDefault();
+    $('#sign-in').modal();
+  });
+  $('.icon-github').on('click', function(e) {
+    e.preventDefault();
+    OAuth.popup('github', function(err, result) {
+      $('#sign-in .modal-body').css('font-size', '1.3em');
+      result.provider = 'github'      
+      $.post('/user/oauth', result, function(data) {
+        $('#sign-in .modal-body').html(JSON.stringify(data));
+      });      
+    });
+  });
+});
+
