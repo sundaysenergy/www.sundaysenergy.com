628ffd07b67feb26b83b7d27989b8660b3b85681
Kai Curry <kai@sundaysenergy.com>
2013-08-30 20:45:15 -0500

    try some new thing login button

--- theme/javascripts/pricing.js
+++ theme/javascripts/pricing.js
@@ -1,25 +1,17 @@
 $('document').ready(function() {
-  $('#get_drupal_hosting').on('click', function(e) {
+  $('#login').on('click', function(e) {
     console.log("got here!");
     e.preventDefault();
-    $('#drupal_hosting').modal();
+    $('#sign-in').modal();
   });
   $('.icon-github').on('click', function(e) {
     e.preventDefault();
     OAuth.popup('github', function(err, result) {
-      $('#drupal_hosting .modal-body').css('font-size', '1.3em');
+      $('#sign-in .modal-body').css('font-size', '1.3em');
       
-      $.ajax({
-        url: 'https://api.github.com/user',
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
-          $('#drupal_hosting .modal-body').html(JSON.stringify(data));
-        },
-        beforeSend: function(xhr) {
-          xhr.setRequestHeader('Authorization', 'token ' + result.access_token);
-        }
-      });
+      $.post('http://v2.api.cape.io/user/oauth', result, function(data) {
+        $('#sign-in .modal-body').html(JSON.stringify(data));
+      });      
     });
   });
 });
